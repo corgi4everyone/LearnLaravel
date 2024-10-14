5543067239665871e1d9a75dcc678f86a89a6ca1
Theikdi Sann <20011012theikdi@gmail.com>
2024-10-14 16:51:04 +0700

    change div to nav in layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -16,11 +16,11 @@
         </style>
     </head>
     <body>
-        <div>
+        <nav>
             <a href="/">home</a>
             <a href="/about">about</a>
             <a href="/contact">contact</a>
-        </div>
+        </nav>
         {{ $slot }}
     </body>
 </html>
